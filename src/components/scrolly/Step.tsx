49faa48ecd0b47f9ecc287b269b3697bfab3ed25
prayudahlah <yudafihan@gmail.com>
2026-08-26 08:32:23 +0700

    fix: mobile layout's content was overlapping

--- src/components/scrolly/Step.tsx
+++ src/components/scrolly/Step.tsx
@@ -12,7 +12,7 @@ export function Step({ id, children, className = '', diagram }: StepProps) {
         <div
             id={id}
             data-diagram={diagram}
-            className={`h-screen lg:scroll-mt-0 flex py-12 lg:py-20 step-enter ${className}`}
+            className={`min-h-[50vh] lg:h-screen lg:scroll-mt-0 flex py-8 lg:py-20 step-enter ${className}`}
         >
             <div className="p-6 lg:p-8 w-full">
                 {children}
